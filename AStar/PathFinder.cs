685f20642adc8b37b093a897ee0266b4e5833377
Paweł Pawelec <kontakt@ppawelec.pl>
2017-12-28 16:23:00 +0100

    Add a star algorithm place for implemenation

---
+++ AStar/PathFinder.cs
@@ -0,0 +1,12 @@
+﻿namespace PathFinder.AStar
+{
+    using Core;
+    
+    public class PathFinder : IPathFinder
+    {
+        public void FindPath(Vertex source, Vertex destination)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
